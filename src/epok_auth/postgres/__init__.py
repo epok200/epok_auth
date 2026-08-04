355ec4871c0f3f5d002b6383f6de3a04123e8284
Fernando Leon Franco <fernanlee2131@gmail.com>
2026-08-04 04:03:09 -0600

    feat: expose the PostgreSQL adapter

---
+++ src/epok_auth/postgres/__init__.py
@@ -0,0 +1,4 @@
+from epok_auth.postgres.store import PostgresAuthStore, PostgresAuthTransaction
+from epok_auth.postgres.tables import SCHEMA, metadata
+
+__all__ = ["PostgresAuthStore", "PostgresAuthTransaction", "SCHEMA", "metadata"]
